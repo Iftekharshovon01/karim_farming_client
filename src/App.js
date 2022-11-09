916b8268e474bf9ff52aa083bffcf9fe1b5a0a0c
Iftekharshovon01 <iftekharshovonseu17@gmail.com>
2022-11-10 00:37:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+
+    }
+  ])
+
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
